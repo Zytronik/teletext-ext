3bdedf58f357a9b67d38260238144b1c8a105cee
Zytronik <omar.oberholzer@gmail.com>
2023-05-23 11:10:04 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-document.onload = () => {
-    scrapeContent();
-};
+scrapeContent();
 
 var pageContents = {};
 
 function scrapeContent(){
-        
+    console.log("test");
+    return pageContents = {
+        "header" : getHeaderContents()
+    };
+}
+
+function getHeaderContents(){
+    let headerLinks = document.querySelectorAll("header a");
+    headerLinks.forEach((link)=>{
+        console.log(link);
+    });
 }
